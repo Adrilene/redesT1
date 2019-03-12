b287f96b41f320195b34f0c47e912ddb1943c5d8
adrilene <adrilenefonseca@gmail.com>
2019-03-12 11:40:05 -0300

    final geral

--- webClient.py
+++ webClient.py
@@ -1,57 +1,57 @@
-import socket
-import time
-import webbrowser
+'''
+	Universidade Estadual do Ceará
+	Ciência da Computação
+	Redes de Computadores
+	Prof: Bruno Araújo Lima
+	Alunos: Adrilene Pessoa da Fonseca - 1381145
+			Ricardo de Carvalho Oliveira Filho - 1368080
+'''
 
-page = 'empty'
+import socket 
 
+#Função que monta a mensagem a ser passada como request, seguindo o padrão HTTP
 def messageRequest():
-    site = input('Insira o Site: ')
-    request = 'GET {} HTTP/1.1\nHost: www.{}.com\nConnection: close\nUser-agent: Chrome/70.0\nAccept-language:pt\n'.format(site,site)
+    file = input('Insira nome arquivo: ')
+    request = 'GET {} HTTP/1.1\n'.format(file)
      
     return request
 
 def Main():
+
+	#Define a porta e o endereço (iguais do server)
 	host = '127.0.0.2'
 	port = 5000
 		
+	#Inicia o socket com uma conexão TCP
 	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	while True:
 		print('Disponível?')
+		
+		#verifica se a conexão é feita
 		try:
 			clientSocket.connect((host, port))
 		except:
 			print('Server offline')
-			time.sleep(1)
-			clientSocket.close()
 			break
 
 		print('Conectado!\n')
-
+		
+		#Tendo conectado, envia o request para o socket
 		mrequest = messageRequest()
 		clientSocket.send(mrequest.encode())
 		print('Aguardando resposta...')
-		time.sleep(1)
 
+		#Recebe a resposta do Server e imprime
 		response = clientSocket.recv(2048).decode()
-
 		print('\nResposta do Server: ')
 		print(response)
-		if(response.split(' ')[1] == 200):
-			page = response.split(' ')[3].split('\n')[0]
-		else:
-			page = response.split(' ')[4]
 
 		break
 
+	#Depois de feita, encerra a conexão
 	print('\nEncerrando conexão...')
 	clientSocket.close()
 
-	if(page != 'empty'):
-		print('\nAbrindo página...')
-		webbrowser.open(page, new=0, autoraise=True)
-
 if __name__ == '__main__':
     Main()
-
-
--- webServer.py
+++ webServer.py
@@ -1,50 +1,75 @@
-import socket
-import time
-from datetime import datetime
+'''
+	Universidade Estadual do Ceará
+	Ciência da Computação
+	Redes de Computadores
+	Prof: Bruno Araújo Lima
+	Alunos: Adrilene Pessoa da Fonseca - 1381145
+			Ricardo de Carvalho Oliveira Filho - 1368080
+'''
 
-listAvailable = ['google','uece']
-today = datetime.today()
+import socket
 
 def Main():
+	#endereço e porta específicos (iguais do client)
 	host = "127.0.0.2"
 	port = 5000
 
+	#Configura o socket e inicia no host e porta específica
 	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serverSocket.bind((host,port))
 
+	#espera requests no socket
 	serverSocket.listen(1)
 	print('Pronto para conexão...')
 
 	while True:
 		
-		try:
-			conn, addr = serverSocket.accept()
-		except socket.error:
-			break			
+		#recebe a conexão de um endereço ( client)
+		conn, addr = serverSocket.accept()
 		
 		print("Conexão de: {}\n".format(str(addr)))
 	
-		data = conn.recv(2048).decode()
-		print('Request recebido:\n'+ data)
-		site = data.split(' ')
+		try:
+			#lê o request recebido e o exibe
+			data = conn.recv(2048).decode()
+			print('Request recebido:\n'+ data)
+			filename = data.split(' ')[1]   #recupera o nome do arquivo
 
-		if not data:
+			#abre o arquivo que foi solicitado e 'avisa' que foi encontrado
+			f = open(filename[0:])
+			outputdata = f.read()
+			print('\nArquivo disponível!')
+			conn.send(('HTTP/1.1 200 OK\r\n\r\n').encode())
+
+			#envia o arquivo para o cliente
+			for i in range(0, len(outputdata)):  
+				conn.send((outputdata[i]).encode())
+			conn.send(("\r\n").encode())
+
+			#tendo enviado, fecha a conexão
+			print('Enviado! Fechando Conexão...')
+			conn.close()
+			serverSocket.close()
 			break
 
-		if (site[1] in listAvailable):
-			print('\nDisponível')
-			responseRequest = 'HTTP/1.1 200 OK pageFound.html\nConnection: close\nDate: {}\nServer: {}\nLast-Modified: diadiadiadiada\nContent-Length: 6821\nContent-Type: text/html\n'.format(today.ctime(),host)
-		else:
-			print('\nNot Found')
-			responseRequest = 'HTTP/1.1 404 Not Found pageNotFound.html'
+		except IOError:
+			#caso o arquivo solicitado não tenha sido encontrado, avisa ao cliente
+			print('\nArquivo não disponível!')
+			conn.send(('HTTP/1.1 404 Not Found\r\n\r\n').encode())
+			
+			#envia o arquivo NotFound
+			f = open('pageNotFound.html')
+			outputdata = f.read()
 
-		conn.send(responseRequest.encode())			
-		print('\nEnviando resposta...')
-		time.sleep(1)
-		print('\nEnviado!')
-		break
+			for i in range(0, len(outputdata)):  
+				conn.send((outputdata[i]).encode())
+			conn.send(("\r\n").encode())
 
-	conn.close()
+			print('Fechando conexão...')
+			conn.close()
+			serverSocket.close()
+			break
+	serverSocket.close()
 
 if __name__ == '__main__':
 	Main()
